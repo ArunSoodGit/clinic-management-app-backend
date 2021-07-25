1135cf1af067d10204181d9f728fe701c10d6fa5
Arun <arun.sood1998@gmail.com>
2021-07-25 15:00:59 +0200

    Init

---
+++ src/main/java/com/example/clinicmanagementappbackend/ClinicManagementAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.clinicmanagementappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagementAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicManagementAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clinicmanagementappbackend/ClinicManagementAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clinicmanagementappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagementAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
